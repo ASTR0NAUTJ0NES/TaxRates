736bc1859c6f81f46267a51ffae0f20e4ba491ee
Drew <36201692+ASTR0NAUTJ0NES@users.noreply.github.com>
2018-10-09 09:53:28 -0400

    Initial commit

---
+++ TaxRates.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class TaxRates here.
+ * 
+ * @author Andrew Albanese 
+ * @version 2016-01-21
+ */
+public class TaxRates
+{
+    public static void main(String[] args)
+    {
+    System.out.println(" Sales Tax Rates ");
+    System.out.println("-----------------");
+    System.out.println("Virginia:   5.30%");
+    System.out.println("Kentucky:   6%   ");
+    System.out.println("New Jersey: 7%   ");
+    System.out.println("California: 7.5% ");
+    System.out.println("Navada:     6.85%");
+}
+}   
